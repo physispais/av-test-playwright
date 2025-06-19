b3760da983f9b04df64cb5b9d9417382a4b3bb6f
Timur <physispais.mail.ru>
2025-06-19 22:09:30 +0300

    Fix bugs

--- tests/av.test.ts
+++ tests/av.test.ts
@@ -1,5 +1,9 @@
 import { test, Page, expect } from '@playwright/test';
 
+const BRAND = 'BMW'
+const MODEL = 'X5'
+const GENERATION_1 = 'G05 · Рестайлинг, 2023…'
+const GENERATION_2 = 'G05, 2018…2023'
 test.describe('поиск bmw x6', () => {
   let page: Page;
 
@@ -11,18 +15,18 @@ test.describe('поиск bmw x6', () => {
 
   test('поиск по параметрам', async () => {
     await page.locator('#p-6-0-2-brand').click();
-    await page.locator("//*[@class='dropdown__listitem']//*[contains(text(), 'BMW')]").click();
+    await page.locator(`//*[@class='dropdown__listitem']//*[contains(text(), '${BRAND}')]`).click();
     await page.locator('#p-6-0-3-model').click();
-    await page.locator("//*[@class='dropdown__listitem']//*[text()='X6']").click();
+    await page.locator(`//*[@class='dropdown__listitem']//*[text()='${MODEL}']`).click();
     await page.waitForResponse(response => response.url().includes('/update') && response.status() === 200);
     await page.locator('#p-6-0-4-generation').dblclick();
-    await page.locator("//*[@class='generation-item']//*[text()='G06 · Рестайлинг, 2023…']").click();
-    await page.locator("//*[@class='generation-item']//*[text()='G06, 2019…2023']").click();
+    await page.locator(`//*[@class='generation-item']//*[text()='${GENERATION_1}']`).click();
+    await page.locator(`//*[@class='generation-item']//*[text()='${GENERATION_2}']`).click();
     await page.locator("//*[@class='filter__title']").click();
     await page.waitForTimeout(1000);
     await page.locator("//*[@class='filter__show-result']").click();
-    while (await page.locator("//*[@class='paging__button']").count() > 0) {
-      await page.locator("//*[@class='paging__button']").click();
+    while (await page.locator("//div[@class='paging__button' and contains(text(), 'Показать ещё')]").count() > 0) {
+      await page.locator("//div[@class='paging__button' and contains(text(), 'Показать ещё')]").click();
       await page.waitForTimeout(1000);
     }
     
@@ -40,10 +44,31 @@ test.describe('поиск bmw x6', () => {
     const fullPriceText = (await mostExpensiveCar.locator('.listing-item__prices').textContent()) || '';
     const expectedMainPrice = fullPriceText.split('≈')[0].trim();
 
-    await mostExpensiveCar.locator('a.listing-item__link').click()
-    const actualFullPrice = (await page.locator('.card__price').textContent()) || '';
-    const actualMainPrice = actualFullPrice.split('≈')[0].trim();
+    const link = mostExpensiveCar.locator('a.listing-item__link');
+    const opensInNewTab = await link.evaluate((el: HTMLAnchorElement) => {
+  return el.tagName === 'A' && el.target === '_blank';
+});
 
-    expect(actualMainPrice).toBe(expectedMainPrice);
+let newPage;
+
+if (opensInNewTab) {
+  const context = page.context();
+  [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    link.click()
+  ]);
+  await newPage.waitForLoadState();
+} else {
+  await Promise.all([
+    page.waitForNavigation(),
+    link.click()
+  ]);
+  newPage = page;
+}
+
+const actualFullPrice = (await newPage.locator('.card__price').textContent()) || '';
+const actualMainPrice = actualFullPrice.split('≈')[0].trim();
+
+expect(actualMainPrice).toBe(expectedMainPrice);
   });
 });
